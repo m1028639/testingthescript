ba1a4128e940c23e5331bb97df0bf6b5a90bc78f
m1028639 <sriprasanna.p@gmail.com>
2016-03-17 14:49:22 +0530

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -16,7 +16,7 @@ cd /home/ubuntu && git clone -b MINDTREE https://mdzafarimam:sana_2004@bitbucket
 
 sudo chmod +x /home/ubuntu/catalyst-puppet/puppet/azure.sh
 
-cd /home/ubuntu/catalyst-puppet/puppet && sudo ./azure.sh 'azure007' 'dmp-central-node' version='v08' node='01' azure_public_hostname='dmp-admin-node.westus.cloudapp.azure.com' azure_public_ip='$azure_public_ip' catalyst_environment='sqa'
+cd /home/ubuntu/catalyst-puppet/puppet && sudo ./azure.sh 'azure007' 'dmp-central-node' version='v08' node='01' azure_public_hostname='dmp-admin-node.centralus.cloudapp.azure.com' azure_public_ip='$azure_public_ip' catalyst_environment='sqa'
 
 
 
