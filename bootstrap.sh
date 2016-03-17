2fb1e8d38544f706496b0ac6f9cbde281537a1ed
m1028639 <sriprasanna.p@gmail.com>
2016-03-17 13:20:12 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 
-!#/bin/bash
+#!/bin/bash
 sudo apt-get update -y
 sudo apt-get install git -y
 
@@ -13,12 +13,12 @@ azure_public_hostname=$5
 
 azure_public_ip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
 
+cd /home/ubuntu
 git clone -b MINDTREE https://mdzafarimam:sana_2004@bitbucket.org/cameosaas/catalyst-puppet.git
 
+sudo chmod +x /home/ubuntu/catalyst-puppet/puppet/azure.sh
 
-sudo chmod +x ~/catalyst-puppet/puppet/azure.sh
-
-sudo sh /home/ubuntu/azure.sh 'azure007' 'dmp-central-node' version='v08' node='01' azure_public_hostname='dmp-central-node.westus.cloudapp.azure.com' azure_public_ip='$azure_public_ip' catalyst_environment='sqa'
+sudo sh /home/ubuntu/catalyst-puppet/puppet/azure.sh 'azure007' 'dmp-central-node' version='v08' node='01' azure_public_hostname='dmp-central-node.westus.cloudapp.azure.com' azure_public_ip='$azure_public_ip' catalyst_environment='sqa'
 
 
 
